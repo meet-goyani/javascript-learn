7c6621e38b0e889377fd42b2a10aee5c22676718
Meet Goyani <msgoyani08@gmail.com>
2023-07-12 22:29:15 +0530

    javascipt data types

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hello");
+// Data Types in JavaScript
+
+/* There are two types of data type */
+
+/* 
+    1) Primitive Data Type : 
+    -> They have their own values.
+
+        a) string // typeof = string
+
+        b) number // typeof = number
+
+        c) null // typeof = object
+            - It indicates intentional absence of data.
+
+        d) undefiend // typeof = undefiend
+
+            - variable has been declared but value is not assigned to it
+
+        e) bigIng // typeof = bigIng
+
+        f) symbol // typeof = symbol
+
+    2) Non Primitive Data type :
+    -> They have reference values
+
+        a) array // typeof = object
+
+        b) object // typeof = object
+        
+*/
